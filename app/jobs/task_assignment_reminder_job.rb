7b6c82bdb3f552ef4cd78bae9c8794ad9641f021
jahera Mallick <jahera4ux@gmail.com>
2024-05-28 18:01:31 +0530

    Update task_assignment_reminder_job.rb
    replace user to assigned_to

--- app/jobs/task_assignment_reminder_job.rb
+++ app/jobs/task_assignment_reminder_job.rb
@@ -5,7 +5,7 @@ class TaskAssignmentReminderJob < ApplicationJob
     assignments_due_today = Task.where(end_date: Date.today)
 
     assignments_due_today.each do |assignment|
-      assignment.user.each do |user|
+      assignment.assigned_to.each do |user|
         TaskAssignmentMailer.assignment_reminder(user, assignment).deliver_now
         Rails.logger.info("Sent reminder email to #{user.email} for task #{assignment.title}")
       end
